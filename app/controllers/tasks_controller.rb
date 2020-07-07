4f9e229b74febc86258ba68491867b38a9317132
taiki.nakanowatari <watari1040@outlook.jp>
2020-07-07 13:17:31 +0000

    tasklist05

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
    before_action :require_user_logged_in
    before_action :set_task, only: [:show, :edit, :update, :destroy]
-   before_action :correct_user,only: [:show,:update, :destroy]
+   before_action :correct_user,only: [:show,:update, :destroytaiki]
      def index
        @tasks = Task.all
        @tasks = current_user.tasks
